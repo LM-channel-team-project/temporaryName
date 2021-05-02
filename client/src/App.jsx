2e3cf06e3c99ab5310b0dabd95df8073cb84b64a
moguri846 <dkffkekahsl1@naver.com>
2021-05-02 16:23:05 +0900

    라우터 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import MainPage from "./view/MainPage/MainPage.jsx";
+import LoginPage from "./view/LoginPage/LoginPage.jsx";
+import RegisterPage from "./view/RegisterPage/RegisterPage.jsx";
+
+import "./resetCss.css";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={MainPage} />
-    </Switch>
+    <div id="wrap">
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+      </Switch>
+    </div>
   );
 }
 
