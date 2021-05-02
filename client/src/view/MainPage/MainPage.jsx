07498d277d6d668f0d86d750f55aa429a8091b39
moguri846 <dkffkekahsl1@naver.com>
2021-05-02 16:25:16 +0900

    메인 페이지 추가

--- client/src/view/MainPage/MainPage.jsx
+++ client/src/view/MainPage/MainPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import Header from "../../components/Header/Header.jsx";
+import NoticeBoard from "../../components/NoticeBoard/NoticeBoard.jsx";
+
 function MainPage() {
-  return <div></div>;
+  return (
+    <div>
+      <>
+        <Header />
+      </>
+      <div className="content">
+        <NoticeBoard />
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
